c0fc64c34704252a9a4a749594e70c08b2bb9f36
Brian <MrChozo@gmail.com>
2018-02-20 16:59:35 -0800

    Update with their full issue

--- date.php
+++ date.php
@@ -1,6 +1,23 @@
 <?php
-$start_date = date('m-d-y', strtotime("first day of january this year"));
+// $start_date = date('m-d-y', strtotime("first day of january this year"));
 
-echo $start_date;
+// echo $start_date;
 
-// 01-01-18
+// // 01-01-18
+
+
+
+
+$total_days = 0;
+$current_date = strtotime("today");
+$start_date = strtotime("first day of january this year");
+
+while ($start_date < $current_date)
+{
+    if (date("N", $start_date) <= 5)
+    {
+	    printf($total_days."\n");
+        $total_days++;
+    }
+	$start_date = $start_date + 86400;
+}
