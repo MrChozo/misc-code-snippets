8671f663a29cc715cb95a80b349e56bc172d0917
Brian <MrChozo@gmail.com>
2018-02-23 12:48:23 -0800

    Add new scope test

--- scopeTest.php
+++ scopeTest.php
@@ -1,15 +1,30 @@
 <?php
 
-function setFoo() {
-	$foo = 'bar';
-	printThing();
-	exit;
-}
+// function setFoo() {
+// 	$foo = 'bar';
+// 	printThing();
+// 	exit;
+// }
 
-function printThing() {
-	printf($foo);
-}
+// function printThing() {
+// 	printf($foo);
+// }
 
-setFoo();
+// setFoo();
 // Error
 // Didn't think so
+
+
+function calc($a, $b) {
+  $c = $a + $b;
+
+  function plus5() {
+    // $c is local and undefined
+    var_dump($c); // NULL
+    return $c + 5;
+  }
+  
+  return plus5();
+}
+
+var_dump(calc(1, 2)); // 5
