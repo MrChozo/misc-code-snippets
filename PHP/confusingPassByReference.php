f2bbc85d2f8d653e59512a18b91f07dc6a83b689
Brian <MrChozo@gmail.com>
2018-04-24 10:53:12 -0700

    Add confusingPassByReference

---
+++ PHP/confusingPassByReference.php
@@ -0,0 +1,19 @@
+<?php
+// Pulled from:
+// https://slack.engineering/taking-php-seriously-cf7a60065329 linked to
+// https://dl.acm.org/citation.cfm?id=2660199 (Figure 2)
+$foo = true;
+if ($foo) {
+function weirdArg(&$x) { $x = 'surprise!'; }
+} else {
+function weirdArg($x) { return $x + 1; }
+}
+
+$init = 13;
+echo "weirdArg(\$init): ";
+var_dump(weirdArg($init));
+echo "\$x: ";
+var_dump($x);
+
+// $foo = true   returns NULL
+// $foo = false  returns 14
