41e26409acb0eb34263e78458c3536a63a5a5ba5
Brian Weston <brianw@webriculture.com>
2020-10-19 18:07:14 -0700

    Fun with OOP

--- PHP/decodeAndPrintQueryString.php
+++ PHP/decodeAndPrintQueryString.php
@@ -1,15 +1,50 @@
 <?php
 
-function decodePrint($queryString)
+class QueryString
 {
-    $out = [];
-    parse_str($queryString, $out);
-    print_r($out);
+    private $inputString;
+    private $decodedArray;
+
+    public function __construct($string)
+    {
+        $this->setInputString($string);
+        $this->setDecodedArray($string);
+    }
+
+    public function getDecodedArray()
+    {
+        return $this->decodedArray;
+    }
+
+    public function getInputString()
+    {
+        return $this->inputString;
+    }
+
+    public function decode($inputString)
+    {
+        $out = [];
+        parse_str($inputString, $out);
+
+        return (is_array($out)) ? $out : [];
+    }
+
+    private function setDecodedArray($string)
+    {
+        $this->decodedArray = $this->decode($string);
+    }
+
+    private function setInputString($string)
+    {
+        $this->inputString = $string;
+    }
 }
 
+
+
 if (! empty($argv[1])) {
-    $queryString = $argv[1];
-    decodePrint($queryString);
+    $queryString = new QueryString($argv[1]);
+    print_r($queryString->getDecodedArray());
 } else {
     echo 'No arg passed - URL query string expected as only argument.';
 }
